060ffab6e76843d8cff4176c54c5713534c1996a
naucye <naucye@qq.com>
2022-08-27 23:56:18 +0800

    increase file transfer

--- gitinstall.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/sh
-echo "Installing ..."
-sudo mkdir -vp /usr/share/NChat
-echo "CLoneing project ..."
-sudo git clone https://github.com/naucye/NChat.git /usr/share/NChat
-sudo cp -cv /usr/share/NChat/bin/* /usr/bin
-echo "Setting permissions ..."
-sudo chmod 755 -v /usr/bin/nchat-server /usr/bin/nchat
-echo "Install NChat success!"
